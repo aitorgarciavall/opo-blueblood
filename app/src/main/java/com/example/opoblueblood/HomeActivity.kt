9ad4f10bd1af798e80914d3c59558867c96507db
aitorgarciavall <aitor.g.v.info@gmail.com>
2023-04-14 20:01:38 +0200

    logout button

--- app/src/main/java/com/example/opoblueblood/HomeActivity.kt
+++ app/src/main/java/com/example/opoblueblood/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.opoblueblood
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 
@@ -25,13 +26,9 @@ class HomeActivity : AppCompatActivity() {
 
     private fun setup(email: String, provider: String){
 
-        val emailTextView: TextView = findViewById(R.id.emailTextView)
-        val providerTextView: TextView = findViewById(R.id.provaiderTextView)
-        val logOutButton: Button = findViewById(R.id.logOutButton)
 
-        title = "Inici"
-        emailTextView.text = email
-        providerTextView.text = provider
+        val logOutButton: ImageView = findViewById(R.id.logOutButton)
+
 
         logOutButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
